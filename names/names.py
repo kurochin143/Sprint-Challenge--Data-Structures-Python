b0c0eb6ebe8ea8155d93f97391a40656f69ad207
Israel dela Cruz <igdcruz42@gmail.com>
2019-09-20 13:17:34 -0500

    added another name search

--- names/names.py
+++ names/names.py
@@ -7,17 +7,21 @@ f = open('names_1.txt', 'r')
 names_1 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
+names_1.sort()
+
 f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
+
+names_2.sort()
 
 # b2 = BinarySearchTree(names_2[0])
 # for n in names_2:
 #     b2.insert(n)
 
-d2 = {}
-for n in names_2:
-    d2[n] = 0
+# d2 = {}
+# for n in names_2:
+#     d2[n] = 0
 
 duplicates = []
 
@@ -32,10 +36,25 @@ duplicates = []
 #     if b2.contains(name_1): # log(n)
 #         duplicates.append(name_1)
 
-# O(n) 0.006 seconds
-for name_1 in names_1: # O(n)
-    if name_1 in d2: # O(1)
-        duplicates.append(name_1)
+# # O(n) 0.006 seconds
+# for name_1 in names_1: # O(n)
+#     if name_1 in d2: # O(1)
+#         duplicates.append(name_1)
+
+# O(n + m) 0.019 seconds
+i1 = 0
+i2 = 0
+while i1 < len(names_1) and i2 < len(names_2):
+    name1 = names_1[i1]
+    name2 = names_2[i2]
+    if name1 == name2:
+        duplicates.append(name1)
+        i1 += 1
+        i2 += 1
+    elif name1 < name2:
+        i1 += 1
+    else:
+        i2 += 1
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
